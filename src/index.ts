820da6df716fd24c26722b01e0639254adfbf00e
Alexander van Trijffel <alexander@structura.ws>
2021-08-30 17:53:42 +0200

    chore: start with unhandled error handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+import './unhandled-error-handling'
 import logger from './log'
 import envVar from './env'
-import './unhandled-error-handling'
 
 logger.info(`Hello world! Running in '${envVar('NODE_ENV')}' mode, port ${envVar('PORT')}`)
