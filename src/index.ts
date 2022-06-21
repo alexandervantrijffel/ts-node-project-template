55ab2f69dc8579fb05adf238b57fac8bb9e435ae
Alexander van Trijffel <alexander@structura.ws>
2022-06-21 13:20:01 +0200

    feat: init dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import dotenv from 'dotenv'
+
 import './unhandled-error-handling'
 import logger from './log'
 import envVar from './env'
+
+dotenv.config()
 
 logger.info(`Hello world! Running in '${envVar('NODE_ENV')}' mode, port ${envVar('PORT')}`)
